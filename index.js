58380d3e17e462aeb98737ac9e5723d915af3bb4
powei <poweitamar@gmail.com>
2023-08-14 12:51:13 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function myNumber(number) {
+    if (number % 2 == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+// Print the function
+console.log(myNumber(7));   
+console.log(myNumber(13)); 
+console.log(myNumber(5));
+
+// A for loop where i greater than 4
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
